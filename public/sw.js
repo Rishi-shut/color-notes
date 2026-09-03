3b574befd0453ea7fd146b28eb0eee9a87754b01
Rishi-shut <mriganksingh7890@gmail.com>
2026-09-03 19:49:30 +0530

    fix: refresh drawing app cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'color-notes-v6';
+const CACHE = 'color-notes-v7';
 const ROOT = new URL('./', self.location.href).pathname;
 const asset = (name) => new URL(name, self.registration.scope).pathname;
 const SHELL = [ROOT, asset('manifest.webmanifest'), asset('icon-192.svg'), asset('icon-512.svg')];
